8ea6301f888aadc2ad3d6362e0161dfd576af7a9
Joe Gallegos <joebgallegos@gmail.com>
2017-09-18 21:07:03 -0600

    Friend-Finder Alpha; Updated ReadMe

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var peopleArray = [
+	{
+		'name' : 'Joe',
+		'photo' : 'https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202017/Cars/May/CR-Cars-Inline-2017-Honda-Civic-Si-f-driving-05-17',
+		'scores' : [
+			3,
+			5,
+			1,
+			4,
+			1,
+			4,
+			5,
+			4,
+			3,
+			5
+		],
+	}
+]
+
+module.exports = peopleArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,30 @@
+var friends = require('../data/friends');
+
+module.exports = function(app){
+
+	app.get("/api/friends", function(req, res){
+		res.json(friends);
+	})
+
+		app.post("/api/friends", function(req,res) {
+			var newResults = (req.body['scores']).map(Number); 
+			var totalDifference = 100;
+			var friendIndex; 
+			for (var i = 0; i <friends.length; i++){
+				var firendResults = friends[i]['scores'];
+	
+				var tempDifference = 0;
+				for(var j=0; j< newResults.length; j++){
+					tempDifference += Math.abs(newResults[j]-firendResults[j]);
+				}
+				if (tempDifference < totalDifference) {
+					totalDifference = tempDifference;
+					friendIndex = i; 
+				}
+			}
+			res.json(friends[friendIndex]);
+			friends.push(req.body); 
+		}); 
+	
+};
+
